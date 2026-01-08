f5982d48b17347332f947e2746e1c6096b772c12
shenalachintha <achinthas666@gmail.com>
2026-01-08 20:07:47 +0530

    create navbar

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Navbar, Hero, About, Experience, Tech, Works, Feedback, Contact, StarsCanvas } from './components';
 
+
 const App = () => {
   return (
     <BrowserRouter>
--- my-app/tailwind.config.cjs
+++ my-app/tailwind.config.cjs
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  mode: "jit",
   theme: {
     extend: {
       colors: {
