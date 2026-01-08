c2410599b4fb3691af49eb377c16a0346723ed65
shenalachintha <achinthas666@gmail.com>
2026-01-08 21:59:52 +0530

    create about part hero component

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -3,9 +3,9 @@ import { styles } from "../styles";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+   <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -17,30 +17,14 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Achintha</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I am a Computer science student focusing on full-stack web development and software engineering<br className='sm:block hidden' />
+             Dedicated to creating efficient and user-friendly applications.   
           </p>
+          
         </div>
       </div>
-
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
-    </section>
+       <compuersCanvas/>
+      </section>
   );
 };
 
