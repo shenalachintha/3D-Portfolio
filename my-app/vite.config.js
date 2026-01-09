4132f2de8898e6a2a3677d374ebe3718ff48a00b
shenalachintha <achinthas666@gmail.com>
2026-01-09 18:58:34 +0530

    install dependencies and config tailwind

---
+++ my-app/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from  "@tailwindcss/vite"
+import Path from 'path'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': Path.resolve(__dirname, './src'),
+    },
+  }, 
+})
