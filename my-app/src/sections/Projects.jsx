3cb3eaa2e4827bb1ca55bfb6aaea38852169e8a4
shenalachintha <achinthas666@gmail.com>
2026-01-16 19:07:55 +0530

    start create project section

--- my-app/src/sections/Projects.jsx
+++ my-app/src/sections/Projects.jsx
@@ -1,7 +1,29 @@
+
 export const Projects = () => {
     return (
-       <section>
-
-       </section>
+      <section id="projects" className="py-32 relative overflow-hidden">
+      {/* Bg glows */}
+      <div className="absolute top-1/4 right-0 w-96 h-96 bg-primary/5 rounded-full blur-3xl" />
+      <div className="absolute bottom-1/4 left-0 w-64 h-64 bg-highlight/5 rounded-full blur-3xl" />
+      <div className="container mx-auto px-6 relative z-10">
+        {/* Section Header */}
+        <div className="text-center mx-auto max-w-3xl mb-16">
+          <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase animate-fade-in">
+            Featured Work
+          </span>
+          <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">
+            Projects that
+            <span className="font-serif italic font-normal text-white">
+              {" "}
+              make an impact.
+            </span>
+          </h2>
+          <p className="text-muted-foreground animate-fade-in animation-delay-200">
+            A selection of my recent work, from complex web applications to
+            innovative tools that solve real-world problems.
+          </p>
+        </div>
+       </div>
+      </section>
     )
 }
